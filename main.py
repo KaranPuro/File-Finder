d64fa9457c7f9dfa459166f0616f88a709a95576
KaranPuro <78454341+KaranPuro@users.noreply.github.com>
2021-02-25 12:46:09 -0600

    Add files via upload
    Updated Version

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
-
-
+# opening statement
+print("put file here")
+#inputs user input into program
+path = os.path.abspath(input())
+size = 0
 # folder path input
 print("Enter folder path")
 path = os.path.abspath('C:\Program Files\Common Files')
@@ -14,14 +17,18 @@ max_size = 0
 # for storing the path to the largest file
 max_file = ""
 
+# for storing the name of the largest file
+file_name = ""
+
 # walking through the entire folder, including subdirectories
 
-for folder, subfolders, files in os.walk(path)
+for folder, subfolders, files in os.walk(path):
     for file in files:
-        size = os.stat(os.path.join('C:\Program Files\Common Files')).st_size
+        size = os.stat(os.path.join(folder,file)).st_size
 
         if size>max_size:
             max_size = size
+            name = folder + file
 
-print("the largest file is: "+max_file)
+print("the largest file is: "+name)
 print('Size: '+str(max_size)+' bytes')
